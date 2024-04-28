9651be1fba2ebca0d7c741698cda637c56046a35
Dhananjay Puranik <dhananjaypuranik8@gmail.com>
2024-04-28 21:47:52 +0530

    final changes done

--- client/src/components/Cancel.js
+++ client/src/components/Cancel.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Cancel = () => {
-  return <div>Cancel</div>;
+  return (
+    <>
+      <div className="card">
+        <h1 style={{ alignSelf: "center" }}>Failed</h1>
+        <p style={{ alignSelf: "center" }}>
+          Your transaction is failed;
+          <br /> we'll be in touch shortly!
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Cancel;
--- client/src/components/Success.js
+++ client/src/components/Success.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+//import "./success.css";
 const Success = () => {
-  return <div>Success</div>;
+  return (
+    <>
+      <div className="card">
+        <h1 style={{ alignSelf: "center" }}>Success</h1>
+        <p style={{ alignSelf: "center" }}>
+          We received your purchase request;
+          <br /> we'll be in touch shortly!
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Success;
